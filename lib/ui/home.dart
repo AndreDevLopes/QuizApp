a85a7e1f7e41e2b343b7c05c7a1878011a0f9547
unknown <andrelopessfla@gmail.com>
2020-04-23 06:35:58 -0300

    app finalizado

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -28,6 +28,8 @@ class _QuizAppState extends State<QuizApp> {
     Question.name("Os Estados Unidos tem em seu territorio 54 Estados", false)
   ];
 
+  int _currentQuestionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,73 +38,113 @@ class _QuizAppState extends State<QuizApp> {
           centerTitle: true,
           backgroundColor: Colors.blueGrey),
       backgroundColor: Colors.blueGrey,
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Image.asset(
-                "images/flag.png",
-                width: 250,
-                height: 180,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.circular(14.4),
-                  border: Border.all(
-                      color: Colors.blueGrey.shade400,
-                      style: BorderStyle.solid),
+      body: Builder(
+        builder: (BuildContext context) => Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  "images/flag.png",
+                  width: 250,
+                  height: 180,
                 ),
-                height: 120,
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      questionBank[0].questionText,
-                      style: TextStyle(fontSize: 16.9, color: Colors.white),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(14.4),
+                    border: Border.all(
+                        color: Colors.blueGrey.shade400,
+                        style: BorderStyle.solid),
+                  ),
+                  height: 120,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        questionBank[_currentQuestionIndex].questionText,
+                        style: TextStyle(fontSize: 16.9, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () => _checkAnswer(),
-                  color: Colors.blueGrey.shade900,
-                  child: Text(
-                    "Verdadeiro",
-                    style: TextStyle(color: Colors.white),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () => _prevQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(Icons.arrow_back, color: Colors.white),
                   ),
-                ),
-                RaisedButton(
-                  onPressed: () => _checkAnswer(),
-                  color: Colors.blueGrey.shade900,
-                  child: Text(
-                    "Falso",
-                    style: TextStyle(color: Colors.white),
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(true, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      "True",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-                RaisedButton(
-                  onPressed: () => _nextQuestion(),
-                  color: Colors.blueGrey.shade900,
-                  child: Icon(Icons.arrow_forward, color: Colors.white),
-                ),
-              ],
-            ),
-            Spacer(),
-          ],
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(false, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      "Falso",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _nextQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
+                  ),
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  _nextQuestion() {}
+  _prevQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex - 1) % questionBank.length;
+    });
+  }
 
-  _checkAnswer() {}
+  _updateQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % questionBank.length;
+    });
+  }
+
+  _nextQuestion() {
+    _updateQuestion();
+  }
+
+  _checkAnswer(bool userChoice, BuildContext context) {
+    if (userChoice == questionBank[_currentQuestionIndex].isCorrect) {
+      final snackBar = SnackBar(
+        backgroundColor: Colors.green,
+        duration: Duration(milliseconds: 500),
+        content: Text("Correto!"),
+      );
+      Scaffold.of(context).showSnackBar(snackBar);
+
+      _updateQuestion();
+    } else {
+      final snackBar = SnackBar(
+        backgroundColor: Colors.redAccent,
+        duration: Duration(milliseconds: 500),
+        content: Text("Errado!"),
+      );
+      Scaffold.of(context).showSnackBar(snackBar);
+      _updateQuestion();
+    }
+  }
 }
